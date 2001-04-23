821b1ef7491b6a580a4bca39d7f36e9a404e0277
ovidiu <ovidiu>
2001-04-23 06:35:46 +0000

    Moved the functionality that invoked the XSLT processor in the AbstractXSLTDebugger class.

--- java/xslt/debugger/xalan/XSLTDebugger.java
+++ java/xslt/debugger/xalan/XSLTDebugger.java
@@ -8,25 +8,26 @@
 
 package xslt.debugger.xalan;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.lang.Runnable;
-import java.util.ArrayList;
-import java.util.Stack;
-import javax.xml.transform.Source;
-import javax.xml.transform.Templates;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import org.apache.xalan.trace.PrintTraceListener;
-import org.apache.xalan.trace.TraceListener;
-import org.apache.xalan.trace.TraceManager;
-import org.apache.xalan.transformer.TransformerImpl;
-import xslt.debugger.AbstractXSLTDebugger;
-import xslt.debugger.Manager;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Source;
 import java.util.TooManyListenersException;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.lang.Runnable;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.File;
+
+import xslt.debugger.Manager;
+import xslt.debugger.AbstractXSLTDebugger;
+import org.apache.xalan.transformer.TransformerImpl;
+import org.apache.xalan.trace.TraceManager;
+import org.apache.xalan.trace.TraceListener;
+import org.apache.xalan.trace.PrintTraceListener;
 
 public class XSLTDebugger extends AbstractXSLTDebugger
 {
@@ -63,51 +64,6 @@ public class XSLTDebugger extends AbstractXSLTDebugger
       manager.getObserver().caughtException(e);
     }
   }
-
-//   public synchronized void run()
-//   {
-//     state = RUNNING;
-//     notifyAll();
-
-//     try {
-//       System.out.println("Xalan XSLTDebugger starting...");
-      
-//       PrintWriter diagnosticsWriter = new PrintWriter(System.err, true);
-//       TransformerFactory tFactory
-//         = new org.apache.xalan.processor.TransformerFactoryImpl();
-
-//       PrintTraceListener traceListener
-//         = new PrintTraceListener(diagnosticsWriter);
-//       traceListener.m_traceTemplates = true;
-//       traceListener.m_traceElements = true;
-//       traceListener.m_traceGeneration = true;
-//       traceListener.m_traceSelection = true;
-      
-//       //      tFactory.setAttribute(FeatureKeys.TRACE_LISTENER, traceListener);
-//       //      tFactory.setAttribute(FeatureKeys.LINE_NUMBERING, Boolean.TRUE);
-
-//       File inFile = new File(xmlFilename);
-//       StreamSource in = new StreamSource(xmlFilename);
-//       StreamResult result = new StreamResult(outStream);
-
-//       String media = null, title = null, charset = null;
-//       Source stylesheet
-//         = tFactory.getAssociatedStylesheet(in, media, title, charset);
-//       String stylesheetId = stylesheet.getSystemId();
-
-//       Transformer transformer = tFactory.newTransformer(stylesheet);
-//       if (transformer != null)
-//         transformer.transform(in, result);
-
-//       manager.getObserver().processorFinished();
-//     }
-//     catch(Exception e) {
-//       manager.getObserver().caughtException(e);
-//     }
-
-//     state = NOT_RUNNING;
-//     notifyAll();
-//   }
 
   public ArrayList getGlobalVariables()
   {
