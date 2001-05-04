44019897d42e48eee4a71ff9d25d4430b19f7b76
ovidiu <ovidiu>
2001-05-04 16:02:36 +0000

    Added copyright notice. Inform of stack frame changes whenever they change as a result of xf or sf command.

--- java/xslt/debugger/cmdline/Controller.java
+++ java/xslt/debugger/cmdline/Controller.java
@@ -4,6 +4,22 @@
     Author: Ovidiu Predescu <ovidiu@cup.hp.com>
     Date: March  7, 2001
 
+    Copyright (C) 2001 Ovidiu Predescu
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+    02111-1307, USA.
  */
 
 package xslt.debugger.cmdline;
@@ -488,6 +504,8 @@ public class Controller
     StyleFrame styleFrame = sourceFrame.getStyleFrame();
     Stack styleFrames = manager.getStyleFrames();
     currentStyleFrame = styleFrames.indexOf(styleFrame);
+    if (observer != null)
+      observer.stackFramesChanged(currentSourceFrame, currentStyleFrame);
   }
 
   public void setCurrentStyleFrame(Vector args)
@@ -512,6 +530,8 @@ public class Controller
     SourceFrame sourceFrame = styleFrame.getSourceFrame();
     Stack sourceFrames = manager.getSourceFrames();
     currentSourceFrame = sourceFrames.indexOf(sourceFrame);
+    if (observer != null)
+      observer.stackFramesChanged(currentSourceFrame, currentStyleFrame);
   }
   
   public void showSourceFrames(Vector args)
