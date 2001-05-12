fd56ee580b49aa828a39b0747857b2821ae25811
ovidiu <ovidiu>
2001-05-12 18:14:56 +0000

    Added support for reporting errors that can occur during parsing and transformation.

--- java/xslt/debugger/AbstractXSLTDebugger.java
+++ java/xslt/debugger/AbstractXSLTDebugger.java
@@ -24,26 +24,37 @@
 
 package xslt.debugger;
 
-
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.Templates;
-import javax.xml.transform.Source;
-
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.net.URLConnection;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.lang.Runnable;
-import java.io.OutputStream;
-import java.io.IOException;
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.Runnable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
 import xslt.debugger.Manager;
+import javax.xml.transform.SourceLocator;
 
 public abstract class AbstractXSLTDebugger implements Runnable
 {
@@ -101,14 +112,26 @@ public abstract class AbstractXSLTDebugger implements Runnable
 
     try {
       TransformerFactory tFactory = getTransformerFactory(manager.forDebug);
-      File inFile = new File(xmlFilename);
-      StreamSource in = new StreamSource(xmlFilename);
-      StreamResult result = new StreamResult(manager.getOutStream());
+      tFactory.setErrorListener(new TrAXErrorListener());
 
+      //      File inFile = new File(xmlFilename);
+      //      StreamSource in = new StreamSource(xmlFilename);
+      System.out.println("run: xmlFilename = " + xmlFilename);
+      
+      //FileInputStream fis = new FileInputStream(xmlFilename);
+      InputSource is = new InputSource(new URL(xmlFilename).toString());
+      SAXSource saxSource = new SAXSource(is);
+      setupXMLReader(saxSource);
+      
       String media = null, title = null, charset = null;
-      Source stylesheet
-        = tFactory.getAssociatedStylesheet(in, media, title, charset);
-      String stylesheetId = stylesheet.getSystemId();
+      Source stylesheetSource
+        = tFactory.getAssociatedStylesheet(saxSource, media, title, charset);
+      // Set an error handler for the stylesheet parser
+      if (stylesheetSource instanceof SAXSource)
+        setupXMLReader((SAXSource)stylesheetSource);
+
+      StreamResult result = new StreamResult(manager.getOutStream());
+      String stylesheetId = stylesheetSource.getSystemId();
 
       // Check for a Templates object already created for this
       // stylesheet
@@ -116,29 +139,47 @@ public abstract class AbstractXSLTDebugger implements Runnable
       Templates template;
       
       if (sheetInfo == null) {
-        sheetInfo = new XSLTSheetInfo(stylesheet);
+        sheetInfo = new XSLTSheetInfo(stylesheetSource);
         sheetCache.put(stylesheetId, sheetInfo);
         template = sheetInfo.template;
       }
       else {
-        template = sheetInfo.getTemplates(stylesheet);
+        template = sheetInfo.getTemplates(stylesheetSource);
       }
 
       Transformer transformer = template.newTransformer();
       prepareTransformerForDebugging(transformer, manager.forDebug);
 
       if (transformer != null)
-        transformer.transform(in, result);
-
-      manager.getObserver().processorFinished();
+        transformer.transform(saxSource, result);
     }
     catch(Exception e) {
       manager.getObserver().caughtException(e);
     }
+    finally {
+      manager.getObserver().processorFinished();
+      state = NOT_RUNNING;
+      action = DO_NOTHING;
+      notifyAll();
+    }
+  }
 
-    state = NOT_RUNNING;
-    action = DO_NOTHING;
-    notifyAll();
+  /**
+   * <code>setupXMLReader</code> sets up an ErrorHandler for a given
+   * SAXSource object.
+   *
+   * @param saxSource the <code>SAXSource</code> object
+   */
+  public void setupXMLReader(SAXSource saxSource)
+    throws SAXException, ParserConfigurationException
+  {
+    XMLReader xmlReader = saxSource.getXMLReader();
+    if (xmlReader == null) {
+      xmlReader = SAXParserFactory.newInstance()
+        .newSAXParser().getXMLReader();
+      saxSource.setXMLReader(xmlReader);
+    }
+    xmlReader.setErrorHandler(new SaxParserErrorHandler());
   }
 
   public synchronized void checkRequestToStop()
@@ -268,14 +309,14 @@ public abstract class AbstractXSLTDebugger implements Runnable
     Templates template = null;
     long lastModified = Long.MIN_VALUE;
 
-    public XSLTSheetInfo(Source stylesheet)
+    public XSLTSheetInfo(Source stylesheetSource)
+      throws TransformerConfigurationException
     {
-      try {
-        TransformerFactory tFactory = getTransformerFactory(manager.forDebug);
-        template = tFactory.newTemplates(stylesheet);
-        lastModified = getLastModified(stylesheet.getSystemId());
-      }
-      catch (TransformerConfigurationException e) {}
+      System.out.println("new XSLTSheetInfo: " + stylesheetSource.getSystemId());
+      
+      TransformerFactory tFactory = getTransformerFactory(manager.forDebug);
+      template = tFactory.newTemplates(stylesheetSource);
+      lastModified = getLastModified(stylesheetSource.getSystemId());
     }
 
     long getLastModified(String stylesheetId)
@@ -305,14 +346,15 @@ public abstract class AbstractXSLTDebugger implements Runnable
       return lastModified;
     }
 
-    public Templates getTemplates(Source stylesheet)
+    public Templates getTemplates(Source stylesheetSource)
+      throws TransformerConfigurationException
     {
-      String stylesheetId = stylesheet.getSystemId();
+      String stylesheetId = stylesheetSource.getSystemId();
       long currentTime = Long.MIN_VALUE;
 
       currentTime = getLastModified(stylesheetId);
 
-      if (currentTime <= lastModified && template != null) {
+      if (template != null && currentTime <= lastModified) {
         // The XSLT sheet has not been modified since the last access,
         // return the cached one
         return template;
@@ -320,17 +362,94 @@ public abstract class AbstractXSLTDebugger implements Runnable
 
       // The XSLT sheet has been modified, create a new template
       // and return it
-      try {
-        TransformerFactory tFactory = getTransformerFactory(manager.forDebug);
-        template = tFactory.newTemplates(stylesheet);
-        lastModified = currentTime;
-      }
-      catch (TransformerConfigurationException e) {
-        System.out.println("Could not create transformer for: "
-                           + stylesheetId);
-      }
+      TransformerFactory tFactory = getTransformerFactory(manager.forDebug);
+      template = tFactory.newTemplates(stylesheetSource);
+      lastModified = currentTime;
 
       return template;
+    }
+  }
+
+  /**
+   * Implementation of the
+   * <code>java.xml.transform.ErrorListener</code> interface in the
+   * TrAX interface, and is used to report error to an
+   * <code>Observer</code> instance.
+   *
+   * @author <a href="mailto:ovidiu@cup.hp.com">Ovidiu Predescu</a>
+   */
+  class TrAXErrorListener implements ErrorListener
+  {
+    public TrAXErrorListener() {}
+    
+    public void error(TransformerException ex)
+      throws TransformerException
+    {
+      reportError("error", ex);
+    }
+
+    public void warning(TransformerException ex)
+      throws TransformerException
+    {
+      reportError("warning", ex);
+    }
+
+    public void fatalError(TransformerException ex)
+      throws TransformerException
+    {
+      reportError("fatal error", ex);
+    }
+
+    public void reportError(String type, TransformerException ex)
+      throws TransformerException
+    {
+      //      System.out.println("TrAXErrorListener " + type + ": " + ex);
+      SourceLocator locator = ex.getLocator();
+      String message = locator.getSystemId() + "\n"
+        + locator.getLineNumber() + ":" + locator.getColumnNumber();
+      TransformerException newEx = new TransformerException(message);
+      newEx.initCause(ex);
+      //      manager.getObserver().caughtException(newEx);
+      throw newEx;
+    }
+  }
+
+  /**
+   * <code>SaxParserErrorHandler</code> is an implementation of the
+   * <code>org.xml.sax.ErrorHandler</code> interface, and is used to
+   * report error to an <code>Observer</code> instance.
+   *
+   * @author <a href="mailto:ovidiu@cup.hp.com">Ovidiu Predescu</a>
+   */
+  class SaxParserErrorHandler implements ErrorHandler
+  {
+    public void error(SAXParseException ex)
+      throws SAXException
+    {
+      reportError("error", ex);
+    }
+
+    public void warning(SAXParseException ex)
+      throws SAXException
+    {
+      reportError("warning", ex);
+    }
+
+    public void fatalError(SAXParseException ex)
+      throws SAXException
+    {
+      reportError("fatal error", ex);
+    }
+
+    public void reportError(String type, SAXParseException ex)
+      throws SAXException
+    {
+      //      System.out.println("SaxParserErrorHandler " + type + ": " + ex);
+      String message = ex.getSystemId() + "\n"
+        + ex.getLineNumber() + ":" + ex.getColumnNumber();
+      SAXException newEx = new SAXException(message, ex);
+      //      manager.getObserver().caughtException(newEx);
+      throw newEx;
     }
   }
 }
